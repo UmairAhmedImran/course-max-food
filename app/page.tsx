b92ccc3de76c05e8c523196ab9a614b50f8feedf
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2026-02-24 19:32:31 +0500

    chore(feat): home page minor UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,41 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
-import { Suspense } from "react";
+import { AuthButton } from '@/components/auth-button';
+import { ThemeSwitcher } from '@/components/theme-switcher';
+import Image from 'next/image';
 
-export default function Home() {
+import { Suspense } from 'react';
+
+export function Page() {
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
-            </div>
-            {!hasEnvVars ? (
-              <EnvVarWarning />
-            ) : (
-              <Suspense>
-                <AuthButton />
-              </Suspense>
-            )}
-          </div>
-        </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
-        </div>
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
+    <>
+      <div className='flex p-4 bg-[#E7E7E7]'>
+        <div className='flex flex-1 gap-x-2 items-center'>
+          <Image
+            src='logo.svg'
+            alt='logo'
+            width={30}
+            height={30}
+          />
+          <p className='font-semibold text-xl dark:text-black'>
+            LMS
           </p>
+        </div>
+        <div className='flex flex-1 gap-x-2 justify-end'>
           <ThemeSwitcher />
-        </footer>
+          <Suspense>
+            <AuthButton />
+          </Suspense>
+        </div>
       </div>
-    </main>
+      <div className='flex items-center justify-center bg-[#fdf3e8]'>
+        <Image
+          src='/student-lesson.jpg'
+          alt='Hero section image'
+          width={1000}
+          height={900}
+        />
+      </div>
+    </>
   );
 }
+
+export default Page;
